1f15b211cad7ce4df8ddcfa6529a4327b519e3e8
yonggyo1125 <yonggyo00@kakao.com>
2022-06-06 17:11:55 +0900

    개발환경 구축, 메이븐, 스프링 DI 강의 작성

---
+++ 6.Spring & Spring Boot(75시간)/1일차(3h) - 개발환경 구축, 메이븐, 스프링 DI/src/main/java/kr/codefty/App.java
@@ -0,0 +1,13 @@
+package kr.codefty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
